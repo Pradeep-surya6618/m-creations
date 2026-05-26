f1925fb7b9b39ef1582a21b04e2cbac15edb5449
[Pradeep-incrix] <[pradeepsurya@incrix.in]>
2026-05-26 19:16:17 +0530

    feat: UI store for cart drawer + mobile menu open state

---
+++ src/store/ui.ts
@@ -0,0 +1,20 @@
+import { create } from "zustand";
+
+export type UIState = {
+  isCartOpen: boolean;
+  isMobileMenuOpen: boolean;
+  openCart: () => void;
+  closeCart: () => void;
+  toggleMobileMenu: () => void;
+  closeMobileMenu: () => void;
+};
+
+export const useUIStore = create<UIState>((set) => ({
+  isCartOpen: false,
+  isMobileMenuOpen: false,
+  openCart: () => set({ isCartOpen: true }),
+  closeCart: () => set({ isCartOpen: false }),
+  toggleMobileMenu: () =>
+    set((state) => ({ isMobileMenuOpen: !state.isMobileMenuOpen })),
+  closeMobileMenu: () => set({ isMobileMenuOpen: false }),
+}));
